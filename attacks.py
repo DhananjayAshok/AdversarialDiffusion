eafd03561f843c3b319cbc69aaad394db5af0d50
Dhananjay Ashok <46792537+DhananjayAshok@users.noreply.github.com>
2023-02-13 21:58:14 +0530

    Update attacks.py

--- attacks.py
+++ attacks.py
@@ -1,4 +1,5 @@
 from torchattacks import PGD, PGDL2
+from numpy.random import choice
 
 ATTACKS = {"pgd": PGD, "pdg_l2": PGDL2}
 
@@ -16,3 +17,21 @@ class ImageAttack:
         attack = self.attack_class(model, **self.params)
         attack.set_normalization_used(mean=list(preprocessing['mean']), std=list(preprocessing['std']))
         return attack(input_batch, true_labels)
+
+    
+class AttackSet:
+    def __init__(self, attacks, models, preprocessings):
+        self.attacks = attacks
+        self.models = models
+        self.preprocessings = preprocessings
+        self.n_models = len(models)
+        assert self.n_models == len(self.preprocessings)
+        
+    def sample(self, input_batch, true_labels):
+        i = choice(range(self.n_models))
+        model = self.models[i]
+        preprocessing = self.preprocessings[i]
+        attack = choice(self.attacks)
+        return attack(model, input_batch, true_labels, preprocessing=preprocessing)
+        
+        
