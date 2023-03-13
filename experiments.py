888f2ff1391c1dd813315ce902b072467076a8fd
Hariharan <hariharan@apples-MacBook-Pro-3.local>
2023-03-13 16:36:19 -0400

    move to on device

--- experiments.py
+++ experiments.py
@@ -32,10 +32,11 @@ def get_diffusion(diff_model_name, mixture_dset, attack_set, num_epochs=5, batch
 
     # load diffpure model.
     attack_model = DiffPure(args, config)
-    attack_model = DummyModel()
+    attack_model = attack_model.to(device)
+    # attack_model = DummyModel()
 
-    # from torchsummary import summary
-    # print(summary(attack_model, input_size=(3, 64, 64)))
+    from torchsummary import summary
+    print(summary(attack_model, input_size=(3, 64, 64)))
 
     # sanity checks.
     # image = torch.from_numpy(np.random.randint(0, 255, size=(5, 3, 64, 64)).astype(np.uint8))
